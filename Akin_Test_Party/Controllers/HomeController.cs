ed0719d4ba314b13626acb10cf669d3a0915bef1
akino <akino@DESKTOP-LQ8220V>
2022-06-22 15:47:41 +0200

    Add project files.

---
+++ Akin_Test_Party/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Akin_Test_Party.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Akin_Test_Party.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static List<VisitorsModel> Visitors = new List<VisitorsModel>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+       
+        [HttpPost]
+        public IActionResult Create(VisitorsModel visitor)
+        {
+            Visitors.Add(visitor);
+            return RedirectToAction(nameof(Create));
+        }
+        public IActionResult Summary()
+        {
+            ViewBag.NumberofGuests = Visitors.Count;
+            ViewBag.NumberOfFamilyGuests = Visitors.Where(x => x.IsFamily).Count();
+            ViewBag.Youngest = Visitors.Min(x => x.Age);
+            ViewBag.Oldest = Visitors.Max(x => x.Age);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Akin_Test_Party/Models/VisitorsModel.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Akin_Test_Party.Models
+{
+    public class VisitorsModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "field must be at least 3 characters and max 20 characters")]
+        [Display(Name = "Your First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(25, MinimumLength = 3, ErrorMessage = "field must be at least 3 characters and max 25 characters")]
+        [Display(Name = "Your Last Name")]
+        public string SecondName { get; set; }
+
+        [EmailAddress(ErrorMessage ="Please enter a proper mail address")]
+        [Display(Name = "Your E-mail Address")]
+        public string EmailAdress { get; set; }
+
+        [Required]
+        [Display(Name = "Your Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Range(18, 60, ErrorMessage = "You can enter your age between 18 - 60")]
+        [Display(Name = "Your Age")]
+        public int Age { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Your Home Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [Range(1000, 15000, ErrorMessage = "You can enter Postal Code between 1000 - 15000")]
+        [Display(Name = "Your Postal Code")]
+        public int PostalCode { get; set; }
+
+        [Required]
+        [Display(Name = "The City Do You Live in")]
+        public string City { get; set; }
+
+        [Display(Name = "Are You a Family?")]
+        public bool IsFamily { get; set; }
+    }
+}
